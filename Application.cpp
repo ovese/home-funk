f3ca92953beef439e6c5871cf652f009e3dd4d3e
ovese <oveseovese@gmail.com>
2022-08-05 04:07:43 +0100

    implementing task 4

--- Application.cpp
+++ Application.cpp
@@ -4,13 +4,16 @@ int main(int argc, char* argv[]){
     TaskPanel tp;
     //tp.ItemizeTasks();
 
-    MontyGame mg;
-    mg.playMonty();
+    //MontyGame mg;
+    //mg.playMonty();
 
     std::cout<<std::endl;
 
-    WarAndPeace wnp;
-    wnp.readTextFile("book-war-and-peace.txt");
+    //WarAndPeace wnp;
+    //wnp.readTextFile("book-war-and-peace.txt");
+
+    TaskFour tf;
+    tf.readFile();
 
     return 0;
 }
